d198c2d090d240e3b18bc7c7282a642c9fb56166
Leonard Gagnon <leonard.gagnon@gmail.com>
2023-12-04 18:43:58 -0500

    Day 3 completed

---
+++ day-03/src/bin/part1.rs
@@ -0,0 +1,137 @@
+use regex::Regex;
+
+fn main() {
+    let input = include_str!("../../input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> String {
+    let re = Regex::new(r"[0-9]+").unwrap();
+
+    let mut total: u32 = 0;
+    let mut line_idx = 0usize;
+    for line in input.lines() {
+        let mut col_idx = 0usize;
+        while col_idx < line.chars().count() {
+            dbg!(col_idx);
+            let col = line.chars().nth(col_idx).unwrap();
+            if col != '.' && !is_symbol(col) {
+                let mat = re.find(&line[col_idx..line.len()]).unwrap();
+                dbg!(mat);
+                if let Ok(number) = mat.as_str().parse::<u32>() {
+                    dbg!(number);
+                    if is_part_number(input, number, line_idx, col_idx) {
+                        println!("Is part number!");
+                        total += number;
+                    }
+                }
+                col_idx += mat.end() - mat.start();
+                continue;
+            }
+            col_idx += 1;
+        }
+        line_idx += 1;
+    }
+
+    total.to_string()
+}
+
+fn is_symbol(c: char) -> bool {
+    match c {
+        '0' => false,
+        '1' => false,
+        '2' => false,
+        '3' => false,
+        '4' => false,
+        '5' => false,
+        '6' => false,
+        '7' => false,
+        '8' => false,
+        '9' => false,
+        '.' => false,
+        _ => true
+    }
+}
+
+fn check_line(line: Option<&str>, col_idx: usize) -> bool {
+    if let Some(line) = line {
+        if col_idx > 0 {
+            if let Some(ch) = line.chars().nth(col_idx - 1) {
+                if is_symbol(ch) {
+                    return true;
+                }
+            }
+        }
+        if let Some(ch) = line.chars().nth(col_idx) {
+            if is_symbol(ch) {
+                return true;
+            }
+        }
+        if col_idx < line.len() - 1 {
+            if let Some(ch) = line.chars().nth(col_idx + 1) {
+                if is_symbol(ch) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+fn check_neighbors(input: &str, line_idx: usize, col_idx: usize) -> bool {
+    if line_idx > 0 && check_line(input.lines().nth(line_idx - 1), col_idx) {
+        return true;
+    }
+    if line_idx < input.lines().count() - 1 && check_line(input.lines().nth(line_idx + 1), col_idx) {
+        return true;
+    }
+    return check_line(input.lines().nth(line_idx), col_idx);
+}
+
+fn is_part_number(input: &str, number: u32, line_idx: usize, col_idx: usize) -> bool {
+    if check_neighbors(input, line_idx, col_idx) {
+        return true;
+    }
+
+    if number >= 10 {
+        if check_neighbors(input, line_idx, col_idx + 1) {
+            return true;
+        }
+    }
+    if number >= 100 {
+        if check_neighbors(input, line_idx, col_idx + 2) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let result = part1(
+            "467..114..\n\
+            ...*......\n\
+            ..35..633.\n\
+            ......#...\n\
+            617*......\n\
+            .....+.58.\n\
+            ..592.....\n\
+            ......755.\n\
+            ...$.*....\n\
+            .664.598..");
+        assert_eq!(result, "4361");
+    }
+
+    #[test]
+    fn test_part1_real_data() {
+        let input = include_str!("../../input1.txt");
+        let result = part1(input);
+        assert_eq!(result, "539713");
+    }
+}
---
+++ day-03/src/bin/part2.rs
@@ -0,0 +1,135 @@
+
+struct ParsedLine {
+    numbers: Vec<(u32, Vec<NumberRange>)>
+}
+
+#[derive(PartialEq, PartialOrd, Eq, Ord, Clone)]
+struct NumberRange {
+    start: usize,
+    end: usize
+}
+
+fn main() {
+    let input = include_str!("../../input1.txt");
+    let output = part2(input);
+    dbg!(output);
+}
+
+fn part2(input: &str) -> String {
+    let parsed_input = parse_input(input);
+
+    let mut line_idx = 0usize;
+    let gears_ratio_totals = input.lines()
+        .map(|line| {
+            let ratios = line.match_indices("*")
+                .filter_map(|(idx, _)| {
+                    extract_gear(&parsed_input, line_idx, idx)
+                })
+                .collect::<Vec<u32>>();
+            line_idx += 1;
+            return ratios;
+        })
+        .reduce(|mut a, mut b| {
+            a.append(&mut b);
+            a
+        }).unwrap();
+
+    gears_ratio_totals.into_iter().sum::<u32>().to_string()
+}
+
+fn parse_input(input: &str) -> Vec<ParsedLine> {
+    let re = regex::Regex::new(r"\b\d+\b").unwrap();
+    input.lines()
+        .map(|line| {
+            let mut numbers = line.split(|c: char| !c.is_digit(10))
+                .filter_map(|v| if !v.is_empty() {
+                    let number = v.parse::<u32>().unwrap();
+                    let mut ranges = vec![];
+                    for mat in re.find_iter(line) {
+                        if mat.as_str() == number.to_string().as_str() {
+                            ranges.push(get_range(mat.start(), v.len()));
+                        }
+                    }
+                    Some((number, ranges))
+                } else {
+                    None
+                })
+                .collect::<Vec<(u32, Vec<NumberRange>)>>();
+            numbers.sort();
+            numbers.dedup();
+            ParsedLine {
+                numbers
+            }
+        })
+        .collect()
+}
+
+fn get_range(idx: usize, len: usize) -> NumberRange {
+    let mut start = idx;
+    if start > 0 {
+        start -= 1;
+    }
+
+    NumberRange {
+        start,
+        end: idx + len
+    }
+}
+
+fn extract_gear(input: &Vec<ParsedLine>, line_idx: usize, col_idx: usize) -> Option<u32> {
+    let mut ratios = vec![];
+    for line_idx in line_idx-1..line_idx+2 {
+        if line_idx < input.len() {
+            ratios.append(&mut input[line_idx].numbers.clone().into_iter()
+                .filter_map(|(number, ranges)| {
+                    for range in ranges {
+                        if col_idx >= range.start && col_idx <= range.end {
+                            return Some(number);
+                        }
+                    }
+                    None
+                })
+                .collect::<Vec<u32>>());
+        }
+    }
+
+    if ratios.len() < 2 {
+        return None;
+    }
+
+    if ratios.len() > 2 {
+        dbg!(ratios.clone());
+        panic!("OUPS!")
+    }
+    //Some(0)
+    Some(ratios.into_iter().reduce(|a, b| a * b).unwrap())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part2() {
+        let result = part2(
+            "467..114..\n\
+            ...*......\n\
+            ..35..633.\n\
+            ......#...\n\
+            617*......\n\
+            .....+.58.\n\
+            ..592.....\n\
+            ......755.\n\
+            ...$.*....\n\
+            .664.598..");
+        assert_eq!(result, "467835");
+    }
+
+    #[test]
+    fn test_part2_real_data() {
+        let input = include_str!("../../input1.txt");
+        let result = part2(input);
+        assert_eq!(result, "84159075");
+    }
+
+}
